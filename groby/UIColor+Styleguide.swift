487c6e12c039a5cab49b75d906bed09317d17bca
JeaSung Lee <wotjdzz1@naver.com>
2018-08-03 22:09:33 +0900

    add colors

---
+++ groby/UIColor+Styleguide.swift
@@ -0,0 +1,48 @@
+//
+//  UIColor+Styleguide.swift
+//  groby
+//
+//  Created by 이재성 on 03/08/2018.
+//  Copyright © 2018 mashup. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var iris: UIColor {
+        return UIColor(red: 81.0 / 255.0, green: 109.0 / 255.0, blue: 196.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var softBlue: UIColor {
+        return UIColor(red: 109.0 / 255.0, green: 139.0 / 255.0, blue: 230.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var powderBlue: UIColor {
+        return UIColor(red: 167.0 / 255.0, green: 187.0 / 255.0, blue: 250.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var lightPeriwinkle: UIColor {
+        return UIColor(red: 204.0 / 255.0, green: 217.0 / 255.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var whiteOne: UIColor {
+        return UIColor(white: 244.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var whiteTwo: UIColor {
+        return UIColor(white: 213.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var coolGrey: UIColor {
+        return UIColor(red: 147.0 / 255.0, green: 147.0 / 255.0, blue: 149.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var brownishGrey: UIColor {
+        return UIColor(white: 96.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var black: UIColor {
+        return UIColor(white: 35.0 / 255.0, alpha: 1.0)
+    }
+    
+}
