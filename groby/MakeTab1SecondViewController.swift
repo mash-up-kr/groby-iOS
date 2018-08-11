04425a22b226f0d5d45087a6f107879b3af02d20
JeaSung Lee <wotjdzz1@naver.com>
2018-08-12 04:26:13 +0900

    image picker 하는데.. richeditor를 어떻게 해야할까요? ㅠㅠㅠㅠ

--- groby/MakeTab1SecondViewController.swift
+++ groby/MakeTab1SecondViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import RichEditorView
+import TLPhotoPicker
 
 class MakeTab1SecondViewController: UIViewController {
-
+    @IBOutlet weak var editor: RichEditorView!
+    lazy var toolbar: RichEditorToolbar = {
+        let toolbar = RichEditorToolbar(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 44))
+        toolbar.options = [RichEditorDefaultOption.image]
+        return toolbar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        editor.placeholder = "내용"
+        toolbar.editor = editor
+        editor.inputAccessoryView = toolbar
+        toolbar.delegate = self
         
         navigationItem.setCustomTitle("내용 작성")
     }
@@ -23,5 +36,32 @@ class MakeTab1SecondViewController: UIViewController {
     @IBAction func nextButtonAction(_ sender: UIButton) {
         guard let makeTab1ThirdTableTableViewController = storyboard?.instantiateViewController(withIdentifier: "MakeTab1ThirdTableTableViewController") as? MakeTab1ThirdTableTableViewController else { return }
         navigationController?.pushViewController(makeTab1ThirdTableTableViewController, animated: true)
+    }
+}
+
+extension MakeTab1SecondViewController: RichEditorToolbarDelegate {
+    func richEditorToolbarInsertImage(_ toolbar: RichEditorToolbar) {
+        let photoPickerViewController = TLPhotosPickerViewController()
+        photoPickerViewController.delegate = self
+        present(photoPickerViewController, animated: true, completion: nil)
+    }
+}
+
+extension MakeTab1SecondViewController: TLPhotosPickerViewControllerDelegate {
+    func dismissPhotoPicker(withTLPHAssets: [TLPHAsset]) {
+        withTLPHAssets.forEach {
+            $0.phAsset?.getURL(completionHandler: { [weak self] url in
+                guard let urlString = url?.absoluteString else { return }
+                self?.toolbar.editor?.insertImage(urlString, alt: "")
+            })
+        }
+    }
+    
+    func handleNoAlbumPermissions(picker: TLPhotosPickerViewController) {
+        // handle denied albums permissions case
+    }
+    
+    func handleNoCameraPermissions(picker: TLPhotosPickerViewController) {
+        // handle denied camera permissions case
     }
 }
