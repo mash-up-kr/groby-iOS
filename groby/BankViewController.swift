06db1e87800d22b87cbdfa707397df7c2645d158
JERCY <wotjdzz1@naver.com>
2018-08-18 17:23:53 +0900

    4: gitflow-feature-stash: jsMain (#8)

---
+++ groby/BankViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BankViewController.swift
+//  groby
+//
+//  Created by 이재성 on 17/08/2018.
+//  Copyright © 2018 mashup. All rights reserved.
+//
+
+import UIKit
+
+class BankViewController: UIViewController {
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
