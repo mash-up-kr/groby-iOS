1d8a9ebd22a17866b0c7ab9b31bac99b95fab451
ByungTak <tak8997@gmail.com>
2018-09-02 17:40:33 +0900

    Feature/write content (#10)
    * remove unnccessary code
    
    * add MakeTab1SecondViewController design framework
    
    * update write_content by pr comment
    
    * resolve pr comment
    
    * add MakeTab1SecondViewController design framework
    
    * update write_content by pr comment
    
    * resolve pr comment

---
+++ groby/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  groby
+//
+//  Created by byungtak on 2018. 8. 18..
+//  Copyright © 2018년 mashup. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageClearButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+}
--- groby/MakeTab1SecondViewController.swift
+++ groby/MakeTab1SecondViewController.swift
@@ -7,10 +7,36 @@
 //
 
 import UIKit
-import RichEditorView
-import TLPhotoPicker
 
 class MakeTab1SecondViewController: UIViewController {
+    @IBAction func nextButtonAction(_ sender: UIButton) {
+        guard let makeTab1ThirdTableTableViewController = storyboard?.instantiateViewController(withIdentifier: "MakeTab1ThirdTableTableViewController") as? MakeTab1ThirdTableTableViewController else { return }
+        navigationController?.pushViewController(makeTab1ThirdTableTableViewController, animated: true)
+    }
+    
+    @IBAction func tappedImgaeFetch(_ sender: UIButton) {
+        let alertController: UIAlertController = UIAlertController()
+        
+        let cameraAction: UIAlertAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.default) { [weak self] (_ : UIAlertAction) in
+            print("camera")
+        }
+        
+        let albumAction: UIAlertAction = UIAlertAction(title: "Album", style: UIAlertActionStyle.default) { [weak self] (_ : UIAlertAction) in
+            guard let `self` = self else {
+                return
+            }
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        
+        alertController.addAction(cameraAction)
+        alertController.addAction(albumAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
     @IBOutlet weak var editor: RichEditorView!
     lazy var toolbar: RichEditorToolbar = {
         let toolbar = RichEditorToolbar(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 44))
@@ -18,6 +44,19 @@ class MakeTab1SecondViewController: UIViewController {
         return toolbar
     }()
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var imageAddButton: UIButton!
+    
+    private lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        
+        return picker
+    } ()
+    
+    private var imageUrls: [URL] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,12 +64,36 @@ class MakeTab1SecondViewController: UIViewController {
         toolbar.editor = editor
         editor.inputAccessoryView = toolbar
         toolbar.delegate = self
-
+      
         navigationItem.setCustomTitle("내용 작성")
+        
+        initializeView()
     }
+    
+    private func initializeView() {
+        imageAddButton.layer.borderWidth = 1
+        imageAddButton.layer.borderColor = UIColor.whiteTwo.cgColor
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        
+        flowLayout.sectionInset = UIEdgeInsets.zero
+        flowLayout.minimumInteritemSpacing = 6
+        flowLayout.minimumLineSpacing = 0
+        
+        let itemHeight = collectionView.frame.size.height
+        let itemWidth = itemHeight
+        
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        
+        collectionView.collectionViewLayout = flowLayout
+    }
+  
+    @objc private func tappedClearImage(sender: UIButton) {
+        let index : Int = sender.layer.value(forKey: "selected_index") as! Int
+        
+        imageUrls.remove(at: index)
+        
+        collectionView.reloadData()
     }
 
     @IBAction func nextButtonAction(_ sender: UIButton) {
@@ -39,22 +102,58 @@ class MakeTab1SecondViewController: UIViewController {
     }
 }
 
-extension MakeTab1SecondViewController: RichEditorToolbarDelegate {
-    func richEditorToolbarInsertImage(_ toolbar: RichEditorToolbar) {
-        let photoPickerViewController = TLPhotosPickerViewController()
-        photoPickerViewController.delegate = self
-        present(photoPickerViewController, animated: true, completion: nil)
+extension MakeTab1SecondViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageUrls.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell: ImageCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "image_cell", for: indexPath) as? ImageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let imageUrl: URL = imageUrls[indexPath.item]
+
+        cell.imageClearButton.layer.setValue(indexPath.row, forKey: "selected_index")
+        cell.imageClearButton.addTarget(self, action: #selector(tappedClearImage), for: .touchUpInside)
+        
+        DispatchQueue.global().async {
+            guard
+                let imageData: Data = try? Data.init(contentsOf: imageUrl as URL),
+                let image: UIImage = UIImage(data: imageData) else {
+
+                return
+            }
+            
+            DispatchQueue.main.async {
+                cell.imageView.image = image
+            }
+        }
+        
+        return cell
     }
 }
 
-extension MakeTab1SecondViewController: TLPhotosPickerViewControllerDelegate {
-    func dismissPhotoPicker(withTLPHAssets: [TLPHAsset]) {
-        withTLPHAssets.forEach {
-            $0.phAsset?.getURL(completionHandler: { [weak self] url in
-                guard let urlString = url?.absoluteString else { return }
-                self?.toolbar.editor?.insertImage(urlString, alt: "")
-            })
+extension MakeTab1SecondViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: false, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let imageUrl = info[UIImagePickerControllerImageURL] else {
+            return
         }
+        
+        guard let url = imageUrl as? URL else {
+            return
+        }
+        
+        imageUrls.append(url)
+        
+        collectionView.reloadData()
+        
+        dismiss(animated: true, completion: nil)
     }
 
     func handleNoAlbumPermissions(picker: TLPhotosPickerViewController) {
@@ -65,3 +164,6 @@ extension MakeTab1SecondViewController: TLPhotosPickerViewControllerDelegate {
         // handle denied camera permissions case
     }
 }
+
+
+
